73f3f35ae4641df20e01041384b2333f332add11
Victor-kay <jvictorkingorikay94@gmail.com>
2023-09-25 01:24:10 -0700

    writing 1-insertion_sort_list.c and 1-O file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *iter, *insert, *tmp;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
+    for (iter = (*list)->next; iter != NULL; iter = tmp)
+    {
+        tmp = iter->next;
+        insert = iter->prev;
+        while (insert != NULL && iter->n < insert->n)
+        {
+            swap_nodes(list, &insert, iter);  /* Use the provided swap_nodes function */
+            print_list(*list);  /* Print the list after each swap */
+        }
+    }
+}
